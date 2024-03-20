9fe0bd7637aa000a3bcf1e42f48a71065d9dbe50
Sobja <jarek.sobon@gmail.com>
2024-03-20 14:31:13 +0100

    Testar banner

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,11 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Banner from "../Images/eBlue-Banner.jpg";
 
 export default function About() {
     return <>
         <Navbar />
+        <img src={Banner} alt="eBlue banner" width="" height=""></img>
         <h1>Home - Under construction</h1>
         <Footer />
 
