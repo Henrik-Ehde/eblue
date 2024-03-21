db34405535b6b6531cb1887d9f5fcf21c44ef411
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-03-21 13:02:20 +0100

    About page created

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import './Navbar.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,30 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Image from '../Images/about.jpg'
+import './About.css';
 
 export default function About() {
     return <>
         <Navbar />
-        <h1>About - Under construction</h1>
+        <div className="About">
+            <img src={Image}></img>
+
+            <div>
+                <h1>About</h1>
+
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget sollicitudin dolor. Morbi finibus elit vitae erat mollis, nec congue massa pretium.  
+                </p>
+
+                <p>
+                Sed eget tellus non sem faucibus ornare. In hac habitasse platea dictumst. Nulla tempor, arcu nec blandit volutpat, felis massa facilisis sem, at imperdiet velit mi ut lorem. Phasellus lacinia facilisis ex, sed varius magna bibendum quis. Donec ut magna in tortor ullamcorper laoreet quis scelerisque massa.
+                </p>
+                
+            </div>
+
+        </div>
+        
+        
         <Footer />
 
     </>
