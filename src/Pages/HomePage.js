845b7bf92c5ed656fd27cbdba5ec7091bb92b691
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-03-22 10:03:12 +0100

    Home page updated

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,19 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Banner from "../Images/eBlue-Banner.jpg";
+import { Link } from 'react-router-dom';
 
 export default function About() {
     return <>
         <Navbar />
         <img src={Banner} alt="eBlue banner" width="" height=""></img>
-        <h1>Home - Under construction</h1>
+        <p className="HomePage">
+            Welcome to eblue!
+            <br />
+            <Link to="/eblue/products">Click here to see our products.</Link>
+
+
+        </p>
         <Footer />
 
     </>
